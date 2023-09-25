5edc8167667d7c1ae68d9e918ae0e5b61712f4a1
Shreehari N W <34746409+shreeharinw@users.noreply.github.com>
2023-09-25 11:09:33 +0530

    Updated source code

--- src/main/java/com/gl/employeeManagementSystem/service/EmployeeService.java
+++ src/main/java/com/gl/employeeManagementSystem/service/EmployeeService.java
@@ -5,5 +5,13 @@ import java.util.List;
 import com.gl.employeeManagementSystem.entity.Employee;
 
 public interface EmployeeService {
+	List<Employee> getAllEmployees();
 
+	Employee saveEmployee(Employee employee);
+
+	Employee getEmployeeById(Long id);
+
+	Employee updateEmployee(Long id, Employee employee);
+
+	void deleteEmployeeById(Long id);
 }
